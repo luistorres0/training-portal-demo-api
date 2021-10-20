f0f36ef5703a81eaba24ba6db813e65e4641dcb0
Luis Torres <luisdtor@gmail.com>
2021-10-20 11:21:13 -0500

    Installed knex and added a .gitignore

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const path = require("path");
+
+const {
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    pool: { min: 1, max: 5 },
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: !!DEBUG,
+  }
