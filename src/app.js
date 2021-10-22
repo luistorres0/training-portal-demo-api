5aa13f93badd08a396057c839ed36a9828f301b7
Luis Torres <luisdtor@gmail.com>
2021-10-21 19:04:31 -0500

    Integrated asyncErrorHandling into 'users' routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 const usersRouter = require("./users/users.router");
 
 const app = express();
@@ -8,5 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,40 +1,33 @@
 const bcrypt = require("bcrypt");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./users.service");
 
 async function create(req, res, next) {
   const { email, password } = req.body.data;
-  try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = { email, password: hashedPassword };
-    await service.create(newUser);
-    res.status(201).send();
-  } catch {
-    res.status(500).json({ message: "Error: Failed to create user" });
-  }
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = { email, password: hashedPassword };
+  await service.create(newUser);
+
+  res.status(201).send();
 }
 
 async function authenticate(req, res, next) {
   const { email, password } = req.body.data;
-
   const foundUser = await service.getUserByName(email);
 
   if (!foundUser) {
-    return res.status(400).json({ message: "Error: User not found" });
+    return next({ status: 404, message: "User not found" });
   }
 
-  try {
-    if (await bcrypt.compare(password, foundUser.password)) {
-      return res.status(200).json({ message: "authenticated" });
-    } else {
-      return res.status(400).json({ message: "not authenticated" });
-    }
-  } catch {
-    return res.status(500).json({ message: "Error: Something went wrong." });
+  if (await bcrypt.compare(password, foundUser.password)) {
+    return res.status(200).json({ message: "authenticated" });
+  } else {
+    return res.status(400).json({ message: "not authenticated" });
   }
 }
 
 module.exports = {
-  create,
-  authenticate,
+  create: [asyncErrorBoundary(create)],
+  authenticate: [asyncErrorBoundary(authenticate)],
 };
